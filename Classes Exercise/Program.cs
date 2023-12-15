79ada3b6b76e5baa2255ad20b525c11a29793d0b
Jon Frederick <JonFred151@gmail.com>
2023-12-15 12:57:10 -0600

    Finish exercise 1

---
+++ Classes Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Classes_Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new Car();
+
+            cars.Make = "Ford";
+            cars.Model = "F150";
+            cars.year = 2016;
+
+            Console.WriteLine($"My truck is a {cars.Make} {cars.Model}! it's year is {cars.year}! ");
+
+        }
+    }
+}
