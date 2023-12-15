a76c98ff4ed68e8e48f67c010922e56bee511395
Jon Frederick <JonFred151@gmail.com>
2023-12-15 14:13:16 -0600

    Exercise 2 Compeleted

---
+++ CarLotSimulator/CarLotSimulator/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public string Make {  get; set; }
+        public int  year {  get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string  IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string EngineNoise)
+        {
+            Console.WriteLine($"{EngineNoise}");
+        }
+
+        public void MakeHonkNoise(string HonkNoise)
+        {
+            Console.WriteLine($"{HonkNoise}");
+        }
+    }
+}
---
+++ CarLotSimulator/CarLotSimulator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Lot = new Car();
+
+            Console.WriteLine("Enter your car Make, Model, Year of your car. \n");
+            Lot.Make = Console.ReadLine();            
+            Lot.Model = Console.ReadLine();            
+            Lot.year = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("What sound does your Engine makes?");
+            Lot.EngineNoise = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What Sound does your Horn make?");
+            Lot.HonkNoise = Console.ReadLine();
+            Console.Clear();
+
+
+            Console.WriteLine($"Your Car is a {Lot.Make} and is a {Lot.Model} from the year {Lot.year}");
+            Lot.MakeEngineNoise(Lot.EngineNoise);
+            Lot.MakeHonkNoise(Lot.HonkNoise);
+
+
+
+
+
+
+
+        }
+    }
+}
